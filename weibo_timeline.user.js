56dc07092930b8b27bc3ccbf55e618c2b8e72416
hemengyang <hmy0119@gmail.com>
2019-07-15 17:28:34 +0800

    添加脚本及说明

---
+++ weibo_timeline.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         让微博按正确的时间线排序
+// @namespace    https://hehome.xyz/
+// @version      0.1.1
+// @icon         https://weibo.com/favicon.ico
+// @description  自动切换到最新微博，恢复正确的时间线
+// @author       hemengyang
+// @match        https://weibo.com/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    var user_id = document.querySelector('a.gn_name').href.split('/')[3];
+    var is_home = window.location.pathname.split('/')[3] == 'home';
+    var is_new = window.location.search.indexOf('is_new') != -1;
+
+    // 自动切换到最新微博
+    if (is_home && !is_new) {
+        window.location = '/home?is_new=1';
+    }
+    // 替换左上角的微博图标点击后网址为最新微博
+    document.querySelector('a.box').setAttribute('href', '/u/' + user_id + '/home?is_new=1');
+})();
